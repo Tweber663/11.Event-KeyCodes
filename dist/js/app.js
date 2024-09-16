49c57e41105983346bc1183a5434f333d6e98e90
Tweber663 <tweber.1995@hotmail.com>
2024-09-16 21:39:13 +0200

    JS added

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,21 @@
-import Stopwatch from './StopwatchWithResults.js';
-new Stopwatch(document.querySelector('.stopwatch'));
+"use strict";
+const keyCode = document.querySelectorAll('span');
+const key = document.querySelector('.key p');
+const code = document.querySelector('.code p');
+const which = document.querySelector('.which p');
+const preInfo = document.querySelector('.preInfo');
+const mainInfo = document.querySelector('.mainInfo');
+window.addEventListener('keydown', (e) => {
+    if (!preInfo.classList.contains('hidden')) {
+        preInfo.classList.add('hidden');
+        mainInfo.classList.remove('hidden');
+    }
+    keyCode.forEach((k) => {
+        k;
+        k.innerText = String(e.keyCode);
+    });
+    key.innerText = String(e.key);
+    code.innerText = String(e.code);
+    which.innerText = String(e.which);
+});
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,24 @@
-import Stopwatch from './StopwatchWithResults.js'
+const keyCode = document.querySelectorAll('span');
+const key = document.querySelector('.key p') as HTMLParagraphElement;
+const code = document.querySelector('.code p') as HTMLParagraphElement; 
+const which = document.querySelector('.which p') as HTMLParagraphElement; 
+const preInfo = document.querySelector('.preInfo') as HTMLDivElement; 
+const mainInfo = document.querySelector('.mainInfo') as HTMLDivElement; 
 
-new Stopwatch(document.querySelector('.stopwatch'))
+
+window.addEventListener('keydown', (e) => {
+    if (!preInfo.classList.contains('hidden')) {
+        preInfo.classList.add('hidden'); 
+        mainInfo.classList.remove('hidden'); 
+    } 
+
+    keyCode.forEach((k) => {
+        k as HTMLSpanElement; 
+        k.innerText = String(e.keyCode);
+    }); 
+    key.innerText = String(e.key)
+    code.innerText = String(e.code); 
+    which.innerText = String(e.which); 
+})
+
+
